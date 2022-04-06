4d9fb1e4e95805c50886f596bcee797054832935
Elliot Juniper <elliotjuniper@gmail.com>
2022-04-06 15:13:00 +0100

    app

---
+++ app.js
@@ -0,0 +1,36 @@
+const dog = document.querySelector("#puppy");
+const cat = document.querySelector("#kitten");
+const squirrel1 = document.querySelector("#squirrel");
+
+const happy = document.querySelector("happy");
+const again = document.querySelector("again");
+const image1 = document.querySelector("image1")
+const state = document.querySelector("state")
+
+const feeding = document.querySelector("feeding");
+const playing = document.querySelector("playing");
+const drinking = document.querySelector("drinking");
+
+let pet;
+let petName;
+
+
+dog.addEventListener("click", () => {
+    image1.src = "./images/puppy.png"
+    pet = new Puppy(petName);
+})
+
+feeding.addEventListener("click", () => {
+    state.style.display = "block";
+    pet.feed();
+})
+
+playing.addEventListener("click", () => {
+    state.style.display = "block";
+    pet.play();
+})
+
+drinking.addEventListener("click", () => {
+    state.style.display = "block";
+    pet.drink();
+})
--- classes.js
+++ classes.js
@@ -1,42 +1,39 @@
 class pet{
     constructor(name){
         this._name = name;
-
-        this._eat = 5;
-        this._thirst = 5;
-        this._play = 5;
     }
     get name(){
         return this._name;
     }
 
-    get eat(){
-        return this._eat;
-        // console.log(`${this.name} is eating`);
-        // this.hunger += hunger.eat;
+    get feed(){
+        happy.value += 2;
+        state.textContent = "OMG this tastes so good!"  
     }    
-    increaseEat(){
-        this._eat++;
+
+    get thirst(){
+        happy.value =+ 1;
+        state.textContent = "I was really thirsty!"
+    }
+
+    get play(){
+        happy.value =+ 3;
+        state.textContent = "I love tennis balls so much!"
+    }
+
+}
+
+class Puppy extends pet {
+    constructor(name) {
+        super(name)
     }
 }
-const molly = new pet("Molly");
-
-molly.increaseEat()
-molly.increaseEat()
-console.log(`${molly.name} is eating and has increase their total hunger to ${molly.eat}`)
 
 
 
 
 
-// class cat extends pet{
-//     constructor(name){
-//         super(name)
-//     }
-//     eatingCat(){
-//         console.log(`this ${this.name} is so greedy!`)
-//     }
-// }
+
 
 
 
