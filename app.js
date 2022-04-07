899fb173ea9cdfd8b147e76f9c36ab3519196781
Ellie Duriez <ellieduriez95@gmail.com>
2022-04-07 23:42:01 +0100

    elliots changes

--- app.js
+++ app.js
@@ -1,27 +1,43 @@
-// GETTING / DEFINING ELEMENTS
 const dog = document.querySelector("#puppy");
 const cat = document.querySelector("#kitten");
 const squirrel1 = document.querySelector("#squirrel");
 
-const happy = document.querySelector("happy");
+const hungerBar = document.querySelector("hungerBar");
+const playBar = document.querySelector("playBar");
+const thirstBar = document.querySelector("thirstBar");
 const again = document.querySelector("again");
-const image1 = document.querySelector("image1")
+const image1 = document.querySelector("pet-img")
 const state = document.querySelector("state")
+const oneName = document.querySelector("#one-name")
 
-const feeding = document.querySelector("feeding");
-const playing = document.querySelector("playing");
-const drinking = document.querySelector("drinking");
+
+const feeding = document.querySelector("#hunger-button");
+const playing = document.querySelector("#play-button");
+const drinking = document.querySelector("#drink-button");
+
 
 let pet;
-let petName;
+let petNameHeader;
 
-//PET SELECTION
+const getNameHeader = () => {
+    petNameHeader = prompt("Please pick the name of your new pet.");
+    }
+
 dog.addEventListener("click", () => {
+    getNameHeader();
     image1.src = "./images/puppy.png"
-    pet = new Puppy(petName);
+    oneName.textContent = pet.name
 })
 
-//GAMEPLAY
+cat.addEventListener("click", () => {
+    image1.src = "./images/kitten.png"   
+})
+
+squirrel1.addEventListener("click", () => {
+    image1.src = "./images/squirell.png"    
+})
+
+
 feeding.addEventListener("click", () => {
     state.style.display = "block";
     pet.feed();
@@ -36,12 +52,3 @@ drinking.addEventListener("click", () => {
     state.style.display = "block";
     pet.drink();
 })
-
-// PAGE FUNCTIONALITY
-drinking.addEventListener("click", () => {
-    state.style.display = "block";
-    pet.drink();
-})
-
-
-
--- classes.js
+++ classes.js
@@ -7,28 +7,27 @@ class pet{
     }
 
     get feed(){
-        happy.value += 2;
+        hungerBar.value += 2;
         state.textContent = "OMG this tastes so good!"  
     }    
 
     get thirst(){
-        happy.value =+ 1;
+        thirstBar.value =+ 1;
         state.textContent = "I was really thirsty!"
     }
 
     get play(){
-        happy.value =+ 3;
+        playBar.value =+ 3;
         state.textContent = "I love tennis balls so much!"
     }
 
 }
 
-class Puppy extends pet {
-    constructor(name) {
-        super(name)
-    }
-}
-
+// class Puppy extends pet {
+//     constructor(name) {
+//         super(name)
+//     }
+// }
 
 
 
