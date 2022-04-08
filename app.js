b327296c56a9de853d4fb7fb55f0ede621f1e773
Ellie Duriez <ellieduriez95@gmail.com>
2022-04-08 03:56:24 +0100

    impletmented care button functionality so that you can now feed the pet and this both updates to the pet instance, and displays to the user in the form of a simple bar graphic.... TO DO: figure out how to put the class in a seperate file to the main one, fix display of image when it is put into the main game (right now it's really big and needs sizing now to always fit the box it's contained in), think about if there's anywhere else that we can maybe create more functions or classes that can handle event listening, creation and anything else for other animals, where we can possibly reduce replication of the code. add different additions for different animals. add style to the website, improve layouting and general appearance. implement different images for different states (i can possibly help with the graphics for this- i have some ideas!). add a timer to the page or some other way to make the status bars decrease.  add a text area to the game page for a state message to be displayed after a user takes an action, so that the pet can express his pleasure/displeasure for the action taken - will need to write an if/switch or some other mechanism in order for this functionality to be implemented. stretch goal:  allow the user to select different items for different animals, with varying levels of benefit.

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 // IMPORTING THE CLASS FILE
 // const {Pet} = require("./classes.js");
-class Pet{
+
+
+class Pet {
     constructor(name, type){
         this._name = name;
         this._type = type;
@@ -8,53 +10,81 @@ class Pet{
     get name(){
         return this._name;
     }
-
     get type(){
         return this._type;
     }
 
-    get feed(){
-        hungerBar.value += 2;
-        state.textContent = "OMG this tastes so good!";
-    }    
+    _hunger = 50
+    _thirst = 50
+    _entertained = 50
 
-    get thirst(){
-        thirstBar.value =+ 1;
-        state.textContent = "I was really thirsty!";
+    get feed(){
+        const feedVal = 10;
+        this._hunger += feedVal;
+        return this._hunger
+        // state.textContent = "OMG this tastes so good!";
+    }
+
+    get quench(){
+        const quenchVal = 10;
+        this._thirst += quenchVal;
+        return this._thirst
+        // state.textContent = "I was really thirsty!";
     }
 
     get play(){
-        playBar.value =+ 3;
-        state.textContent = "I love tennis balls so much!";
+        const playVal = 10;
+        this._entertained += playVal;
+        return this._entertained
+        // state.textContent = "I love tennis balls so much!";
     }
 }
 
-// HOME/SELECTOR PAGE
+// DEFINING ELEMENT... HOME/SELECTOR PAGE
 const catSelectorImage = document.getElementById("catSelectorImage");
 const dogSelectorImage = document.getElementById("dogSelectorImage");
 const squirrelSelectorImage = document.getElementById("SquirrelSelectorImage");
 const homePage = document.getElementById("homePage");
 
-// MAIN GAME PAGE
+// DEFINING ELEMENTS... MAIN GAME PAGE
 const gamePage = document.getElementById("gamePage");
 
 const petName = document.getElementById("petName");
-const hungerBar = document.getElementById("hungerBar");
-const playBar = document.getElementById("playBar");
-const thirstBar = document.getElementById("thirstBar");
+const fedBar = document.getElementById("fedBar");
+const entertainedBar = document.getElementById("entertainedBar");
+const quenchedBar = document.getElementById("quenchedBar");
 
-const mainPetImage = document.getElementById("mainPetImage")
+const mainPetImage = document.getElementById("mainPetImage");
 
+const feedButton = document.getElementById("feedButton");
+const drinkButton = document.getElementById("drinkButton");
+const playButton = document.getElementById("playButton");
+
+let chosenPetObj //THIS WILL STORE THE INSTANCE OF PET THAT THE USER ENDS UP CHOOSING
+
+// SELECING THE PET / INITIALISING INSTANCE
 dogSelectorImage.addEventListener("click", () => {
     homePage.style.display = "none"; // ERASES THE HOMEPAGE
     let name = window.prompt("Please pick the name of your new pet.");
     gamePage.removeAttribute("id") // MAKES THE GAMEPAGE SHOW
-    let chosenPetObj = new Pet(name,"dog"); // CREATES A PET OBJ INSTANCE WITH NAME AND TYPE
+    chosenPetObj = new Pet(name,"dog"); // CREATES A PET OBJ INSTANCE WITH NAME AND TYPE
     mainPetImage.style.backgroundImage = "url(./images/puppy.png)";
-    
     petName.innerText = chosenPetObj.name;
 })
 
+// INTERACT/CARE BUTTON FUNCTIONALITY
+feedButton.addEventListener("click", () => {
+    fedBar.style.width = String(chosenPetObj.feed)+"%";
+})
+
+playButton.addEventListener("click", () => {
+    entertainedBar.style.width = String(chosenPetObj.play)+"%";
+})
+
+drinkButton.addEventListener("click", () => {
+    console.log("ok")
+    quenchedBar.style.width = String(chosenPetObj.quench)+"%";
+})
 
 // feeding.addEventListener("click", () => {
 //     state.style.display = "block";
