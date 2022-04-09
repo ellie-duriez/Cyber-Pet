a17bfda49b4de3b5e3d984535eb6c927987956f0
Ellie Duriez <ellieduriez95@gmail.com>
2022-04-09 15:16:51 +0100

    put the pet class back into it's own js file. All working well.

--- app.js
+++ app.js
@@ -1,25 +1,3 @@
-// IMPORTING THE CLASS FILE
-// const {Pet} = require("./classes.js");
-
-
-class Pet {
-    constructor(name, type){
-        this._name = name;
-        this._type = type;
-    }
-    get name(){
-        return this._name;
-    }
-    get type(){
-        return this._type;
-    }
-
-    _hunger = 50
-    _thirst = 50
-    _entertained = 50
-    
-}
-
 class Fox extends Pet {
     constructor(name) {
        super(name)
--- classes.js
+++ classes.js
@@ -1,46 +1,17 @@
-// class Pet{
-//     constructor(name, type){
-//         this._name = name;
-//         this._type = type;
-//     }
-//     get name(){
-//         return this._name;
-//     }
+class Pet {
+    constructor(name, type){
+        this._name = name;
+        this._type = type;
+    }
+    get name(){
+        return this._name;
+    }
+    get type(){
+        return this._type;
+    }
 
-//     get type(){
-//         return this._type;
-//     } 
-
-//     get feed(){
-//         hungerBar.value += 2;
-//         state.textContent = "OMG this tastes so good!";
-//     }    
-
-//     get thirst(){
-//         thirstBar.value =+ 1;
-//         state.textContent = "I was really thirsty!";
-//     }
-
-//     get play(){
-//         playBar.value =+ 3;
-//         state.textContent = "I love tennis balls so much!";
-//     }
-// }
-
-// module.exports = {
-//     Pet
-// };
-
-// class Puppy extends pet {
-//     constructor(name) {
-//         super(name)
-//     }
-// }
-
-
-
-
-
-
-
-
+    _hunger = 50
+    _thirst = 50
+    _entertained = 50
+    
+}
